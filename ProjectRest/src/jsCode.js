f0d728f930fbfd2de1aa1dc77ffc556af917b556
Peandor <90767819+Peandor@users.noreply.github.com>
2021-11-04 12:23:20 +0500

    Add files via upload

--- ProjectRest/src/jsCode.js
+++ ProjectRest/src/jsCode.js
@@ -4,42 +4,44 @@ var b = prompt("Дизайн 0- Классический , 1- Индивидуа
 
 var c = prompt("Адаптивность 0- Адаптивный, 1-Не адаптивный");
 
-//var arr = ["корпоративный","интернет-магазин","визитка"];
-
-function calculator(money){
-//var a1,b1,c1;
+function calculator(){
+let a1;
+    array1 = ["корпоративный","интернет-магазин","визитка"];
+    array2 = ["Классический","Индивидуальный"];
+    array3 = ["Адаптивный","Не адаптивный"];
 var money=0;
     if(a==0){
-        //a1=mass[0][0];
+        a1=a1+" "+array1[0];
         money=money+2000;
     }
     if(a==1){
-        //a1=mass[0][1];
+        a1=a1+" "+array1[1];
         money=money+3000;
     }
     if(a==2){
-       // a1=mass[0][2];
+       a1=a1+" "+array1[2];
         money=money+1000;
     }
     
     if(b==0){
-       // b1=mass[1][0];
+       a1=a1+" "+array2[0];
         money=money+2000;
     }
     if(b==1){
-       // b1=mass[1][1];
+       a1=a1+" "+array2[1];
         money=money+1000;
     }
     
      if(c==0){
-        //c1=mass[2][0];
+        a1=a1+" "+array3[0];
          money=money+3000;
     }
     if(c==1){
-        //c1=mass[2][1];
+        a1=a1+" "+array3[1];
         money=money+1000;
-    }
-    
+    } 
+    money=money+a1;
+    return money;   
 }
 
 alert(calculator());
